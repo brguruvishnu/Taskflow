2b75b9565eae09fbd5145be1f64b7798bbb0f169
Teju <brguruvishnu998@gmail.com>
2026-05-17 10:23:42 +0530

    Fix Healthcheck failure: bind to 0.0.0.0 and remove npx from start script

--- server/src/app.js
+++ server/src/app.js
@@ -36,6 +36,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
